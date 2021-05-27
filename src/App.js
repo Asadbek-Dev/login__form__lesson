ae1cc02961cccb809a60b07b83dd59ba9236e7c2
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-05-27 11:05:15 +0500

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -12,10 +12,20 @@ function App() {
 
   const Login=details=>{
     console.log(details);
+    if(details.email==adminUser.email && details.password == adminUser.password){
+      console.log("Logged in");
+      setUser({
+        name:details.name,
+        email:details.email
+      })
+    }else {
+      console.log("Details do not match!");
+      setError('Details do not match!');
+    }
   }
 
     const Logout=()=>{
-      console.log("Logout")
+      setUser({name:'',email:''})
     }
   return (
     <div className="App">
@@ -23,7 +33,7 @@ function App() {
         (user.email != "") ? (
           <div className='welcome'>
             <h2>Welcome, <span>{user.name}</span></h2>
-            <button>Logout</button>
+            <button onClick={Logout}>Logout</button>
           </div>
         ):(
           <LoginForm Login={Login} error={error} />
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -14,6 +14,7 @@ const LoginForm = ({Login,error}) => {
         <form onSubmit={submitHandler}>
             <div className='form-inner'>
                 <h2>Login</h2>
+                {(error != '') ? (<div className='error'>{error}</div>):''}
                 <div className='form-group'>
                     <label>Name:</label>
                     <input type='text' name='name' id='name' onChange={e=>setDetails({...details,name:e.target.value})} value={details.name}/>
